8bdbb68097b8d435090740a8ff7cf33266bb7bca
Yasuhiro Niji <awoni@awoni.net>
2016-06-11 23:32:46 +0900

    TKRainWebをASP.NET Core 1.0 RC2 に対応

--- src/TKRainWeb/Gulpfile.js
+++ src/TKRainWeb/Gulpfile.js
@@ -1,16 +1,12 @@
-﻿/// <binding ProjectOpened='watcher' />
-/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
+﻿/// <binding Clean='clean' />
+"use strict";
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var rimraf = require("rimraf");
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var minify = require('gulp-minify-css');
+var cssmin = require("gulp-cssmin");
 
 gulp.task('default', function () {
     gulp.src(['bower_components/bootstrap/dist/*/*'])
@@ -28,7 +24,7 @@ gulp.task("TypeScript", function () {
 
 gulp.task("css", function () {
     gulp.src(['css/*.css'])
-        .pipe(minify())
+        .pipe(cssmin())
         .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest('wwwroot/css'))
 })
--- src/TKRainWeb/Startup.cs
+++ src/TKRainWeb/Startup.cs
@@ -2,27 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TKRainWeb
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            /*
-            app.Run(async (context) =>
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-            */
         }
     }
 }
