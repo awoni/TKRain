ab2797d529286fb98d62913f104b5549d9ea5420
Yasuhiro Niji <awoni@awoni.net>
2016-06-11 22:44:32 +0900

    TestをxUnitに変更

---
+++ src/TKRainTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using TKRain;
+
+namespace TKRainTest
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData(0)]
+        public void TestMethod1(int value)
+        {
+            double lat, lng;
+            XyToBl.Calcurate(4, 10000, 10000, out lat, out lng);
+            Assert.True(Math.Abs(lat - 33.09013) < 0.00001);
+            Assert.True(Math.Abs(lng - 133.60713) < 0.00001);
+            XyToBl.Calcurate(4, 98000, 123000, out lat, out lng);
+            Assert.True(Math.Abs(lat - 33.87650) < 0.00001);
+            Assert.True(Math.Abs(lng - 134.82955) < 0.00001);
+            XyToBl.Calcurate(4, -123000, -111000, out lat, out lng);
+            Assert.True(Math.Abs(lat - 31.88531) < 0.00001);
+            Assert.True(Math.Abs(lng - 132.32669) < 0.00001);
+        }
+    }
+}
--- src/TKRainTest/Properties/AssemblyInfo.cs
+++ src/TKRainTest/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
-[assembly: AssemblyTitle("TKGMapTest")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TKGMapTest")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("TKRainTest")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照できなくなります。このアセンブリ内で COM から型にアクセスする必要がある場合は、 
-// その型の ComVisible 属性を true に設定してください。
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
-[assembly: Guid("1341373e-96a4-4cd3-b42b-3dd2e34caf8c")]
-
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// すべての値を指定するか、以下のように '*' を使用してビルド番号とリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffd9cc5b-2aa2-46fd-b191-a1258060050d")]
