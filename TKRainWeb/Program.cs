33dcbc78ea28bd78fcbd6c0ff245406f98777ff4
awoni <teku@awoni.net>
2018-05-27 00:53:14 +0900

    パッケージのアップデート

--- TKRainWeb/Program.cs
+++ TKRainWeb/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace TKRainWeb
@@ -11,14 +7,13 @@ namespace TKRainWeb
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
